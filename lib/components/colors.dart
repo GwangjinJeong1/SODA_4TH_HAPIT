5968a9d6530feae9b41d14a9d764725d3863f857
GwangjinJeong1 <dbsusqn12@gmail.com>
2023-08-16 23:32:55 +0900

    add colors.dart

---
+++ lib/components/colors.dart
@@ -0,0 +1,35 @@
+import 'dart:ui';
+
+class AppColors {
+  //Background Colors
+  static const Color backgroud1 = Color(0xffF9FAFA);
+  static const Color background2 = Color(0xffF4F4F4);
+  static const Color divider = Color(0xffD6DCDC);
+  static const Color alarm = Color(0xff003339);
+  //Text Colors
+  static const Color mainText = Color(0xff0E0F0E);
+  static const Color bodyText1 = Color(0xff404240);
+  static const Color bodyText2 = Color(0xff999F9B);
+  static const Color textDisable = Color(0xffE0E2DF);
+  static const Color textReversed = Color(0xffFDFDFD);
+  //with Friend
+  static const Color friendOff = Color(0xffC7F1D9);
+  static const Color friendOn = Color(0xff7FDBA4);
+  static const Color friendPlus = Color(0xff85E8AD);
+  //Alone
+  static const Color aloneOff = Color(0xffBBEAF0);
+  static const Color aloneOn = Color(0xff88DFEB);
+  static const Color alonePlus = Color(0xff8EE8F4);
+  //월별보기 달성률
+  static const Color month0 = Color(0xffF6F6F6);
+  static const Color month1 = Color(0xffE4F6F9);
+  static const Color month2 = Color(0xffB0F1FA);
+  static const Color month3 = Color(0xff61DAEA);
+  static const Color month4 = Color(0xff00B0C7);
+  static const Color month5 = Color(0xff008496);
+  //Button Colors
+  static const Color button1 = Color(0xffEDEDED);
+  static const Color button2 = Color(0xff00B0C7);
+  static const Color buttonStroke = Color(0xff0E0F0E);
+  static const Color alert = Color(0xffE83E3E);
+}

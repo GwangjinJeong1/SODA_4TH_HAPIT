46aee0bdcd444421f682b245ae08cfc24c82fc56
GwangjinJeong1 <dbsusqn12@gmail.com>
2023-08-17 10:37:18 +0900

    Edit colors

--- lib/components/colors.dart
+++ lib/components/colors.dart
@@ -12,21 +12,26 @@ class AppColors {
   static const Color bodyText2 = Color(0xff999F9B);
   static const Color textDisable = Color(0xffE0E2DF);
   static const Color textReversed = Color(0xffFDFDFD);
-  //With Friend
+  //with Friend
   static const Color friendOff = Color(0xffC7F1D9);
-  static const Color friendOn = Color(0xff7FDBA4);
+  static const Color friendOn = Color(0xff7EE4A7);
   static const Color friendPlus = Color(0xff85E8AD);
   //Alone
   static const Color aloneOff = Color(0xffBBEAF0);
-  static const Color aloneOn = Color(0xff88DFEB);
+  static const Color aloneOn = Color(0xff77DEEC);
   static const Color alonePlus = Color(0xff8EE8F4);
   //월별보기 달성률
-  static const Color month0 = Color(0xffF6F6F6);
-  static const Color month1 = Color(0xffE4F6F9);
-  static const Color month2 = Color(0xffB0F1FA);
-  static const Color month3 = Color(0xff61DAEA);
-  static const Color month4 = Color(0xff00B0C7);
-  static const Color month5 = Color(0xff008496);
+  static const Color monthBlue0 = Color(0xffF6F6F6);
+  static const Color monthBlue1 = Color(0xffE1FBFF);
+  static const Color monthBlue2 = Color(0xffACF4FF);
+  static const Color monthBlue3 = Color(0xff59E9FF);
+  static const Color monthBlue4 = Color(0xff01CDEC);
+  static const Color monthBlue5 = Color(0xff00A8C1);
+  static const Color monthGreen1 = Color(0xffE2FEED);
+  static const Color monthGreen2 = Color(0xffC0FEDA);
+  static const Color monthGreen3 = Color(0xff65EF9C);
+  static const Color monthGreen4 = Color(0xff46D681);
+  static const Color monthGreen5 = Color(0xff00A744);
   //Button Colors
   static const Color button1 = Color(0xffEDEDED);
   static const Color button2 = Color(0xff00B0C7);

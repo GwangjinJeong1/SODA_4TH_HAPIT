78d1b081ffe7dee45dccb75f283b19f1d9132233
22100584 <103041656+22100584@users.noreply.github.com>
2023-08-17 10:10:00 +0900

    add textStyle

---
+++ lib/components/textStyle.dart
@@ -0,0 +1,23 @@
+// ignore: file_names
+import 'dart:ui';
+
+class AppTextstyle {
+  static TextStyle head1 = TextStyle(
+      fontFamily: 'SpoqaHanSansNeo', fontSize: 36, fontWeight: FontWeight.w700);
+  static TextStyle head2 = TextStyle(
+      fontFamily: 'SpoqaHanSansNeo', fontSize: 24, fontWeight: FontWeight.w700);
+  static TextStyle head3 = TextStyle(
+      fontFamily: 'SpoqaHanSansNeo', fontSize: 21, fontWeight: FontWeight.w700);
+  static TextStyle body1 = TextStyle(
+      fontFamily: 'SpoqaHanSansNeo', fontSize: 18, fontWeight: FontWeight.w500);
+  static TextStyle body2 = TextStyle(
+      fontFamily: 'SpoqaHanSansNeo', fontSize: 18, fontWeight: FontWeight.w400);
+  static TextStyle body3 = TextStyle(
+      fontFamily: 'SpoqaHanSansNeo', fontSize: 15, fontWeight: FontWeight.w500);
+  static TextStyle sub1 = TextStyle(
+      fontFamily: 'SpoqaHanSansNeo', fontSize: 17, fontWeight: FontWeight.w500);
+  static TextStyle sub2 = TextStyle(
+      fontFamily: 'SpoqaHanSansNeo', fontSize: 15, fontWeight: FontWeight.w400);
+  static TextStyle sub3 = TextStyle(
+      fontFamily: 'SpoqaHanSansNeo', fontSize: 13, fontWeight: FontWeight.w500);
+}
